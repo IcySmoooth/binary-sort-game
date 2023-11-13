9788057f81b4412f6a2e7e6e9a69ebcfd1316880
IcySmoooth <90065902+IcySmoooth@users.noreply.github.com>
2023-11-13 00:20:08 -0800

    Created bit array movement state

--- docs/binarysort/main.js
+++ docs/binarysort/main.js
@@ -47,6 +47,7 @@ let playerBit;
 /**
 *@typedef {{
 *	 activated: Boolean
+*  blank: Boolean
 *  expectedValue: Boolean
 *  givenValue: Boolean
 *  pos: Vector
@@ -57,6 +58,24 @@ let playerBit;
 * @type { BitContainer [] }
 */
 let bitContainerArray;
+
+/**
+* @type { BitContainer }
+*/
+let grabBitContainer;
+
+// Timer class. Manages timer based events. Time is measured in frames.
+/**
+*@typedef {{
+  *	 timeLimit: Number
+  *  elapsedTime: Number
+  * }} Timer
+  */
+  
+  /**
+  * @type { Timer }
+  */
+  let timer;
 
 // Implementing a finite state machine to handle game state logic
 /**
@@ -87,6 +106,21 @@ const states = {
 */
 let fsm;
 
+// Variables pertaining to game state management
+/**
+* @type { Vector [] }
+*/
+let bitPositionArray;
+
+/**
+* @type { Vector }
+*/
+let grabBitStartPosition = vec(50, 75);
+/**
+* @type { Vector }
+*/
+let grabBitEndPosition = vec(50, 25);
+
 
 function update() {
   if (!ticks) {
@@ -96,6 +130,28 @@ function update() {
 		};
 
     bitContainerArray = [];
+
+    grabBitContainer = {
+      activated: true,
+      blank: true,
+      expectedValue: false,
+      givenValue: false,
+      pos: vec(grabBitStartPosition.x, grabBitStartPosition.y)
+    }
+
+    bitPositionArray = [
+      vec(2, 75),
+      vec(26, 75),
+      vec(50, 75),
+      vec(74, 75),
+      vec(98, 75),
+      vec(122, 75)
+    ];
+
+    timer = {
+			timeLimit: 0,
+      elapsedTime: 0
+		};
 
     fsm = {
 			currentState: states.MOVE_BIT_ARRAY,
@@ -129,8 +185,19 @@ function update() {
       moveBitArrayEnter: function () {
       },
       moveBitArrayExit: function () {
+        removeFirstBit();
+        pushNewBit(bitPositionArray[5], false);
+        for (let i = 0; i < bitContainerArray.length; i++) {
+          /**
+          * @type { BitContainer }
+          */
+          let bit = bitContainerArray[i];
+          bit.pos = vec(bitPositionArray[i].x, bitPositionArray[i].y);
+        }
       },
       grabBitAscendEnter: function () {
+        setTimer(60);
+        console.log("entering");
       },
       grabBitAscendExit: function () {
       },
@@ -141,19 +208,26 @@ function update() {
 		};
 
     initializeBitArray();
+    setTimer(60);
+    console.log(bitContainerArray);
   }
 
+  // If a timer is active, increment it until it reaches its limit
+  if (timer.elapsedTime >= timer.timeLimit) {
   // Check which state the fsm is in to run its update function
-  switch (this.currentState){
-    case states.MOVE_BIT_ARRAY:
-      this.moveBitArrayUpdate();
-      break;
-    case states.GRAB_BIT_ASCEND:
-      this.grabBitAscendUpdate();
-      break;
-    case states.GRAB_BIT_DESCEND:
-      this.grabBitDescendUpdate();
-      break;
+    switch (fsm.currentState){
+      case states.MOVE_BIT_ARRAY:
+        moveBitArrayUpdate();
+        break;
+      case states.GRAB_BIT_ASCEND:
+        grabBitAscendUpdate();
+        break;
+      case states.GRAB_BIT_DESCEND:
+        grabBitDescendUpdate();
+        break;
+    }
+  } else {
+    timer.elapsedTime++;
   }
   
   // If the input is pressed, the player bit activates to a 1. Else, it stays deactivated as a 0.
@@ -164,7 +238,19 @@ function update() {
 }
 
 function moveBitArrayUpdate() {
+  // Update the positions of all containers in the array
+  for (let i = 0; i < bitContainerArray.length; i++) {
+    /**
+    * @type { BitContainer }
+    */
+    let bit = bitContainerArray[i];
+    bit.pos.x -= 0.5;
+  }
 
+  // Check if middle container is in the correct spot
+  if (bitContainerArray[3].pos.x <= bitPositionArray[2].x) {
+    fsm.changeState(states.GRAB_BIT_ASCEND);
+  }
 }
 
 function grabBitAscendUpdate() {
@@ -180,27 +266,37 @@ function initializeBitArray() {
   * @type { Vector }
   */
   let bitPosition = vec(2, 75);
-  for (let i = 0; i < 5; i++) {
-    pushNewBit(bitPosition);
+  for (let i = 0; i < 6; i++) {
+    if (i < 3){
+      pushNewBit(bitPosition, true);
+    } else{
+      pushNewBit(bitPosition, false);
+    }
     bitPosition.x += 24;
   }
 }
 
 /**
  * @param {Vector} newPos
+ * @param {Boolean} blank
  */
 // Creates a new bit and pushes it to the bit array
-function pushNewBit(newPos) {
+function pushNewBit(newPos, blank) {
   /**
   * @type { BitContainer }
   */
   let newBit = {
     activated: false,
+    blank: blank,
     expectedValue: Math.random() < 0.5,
     givenValue: false,
     pos: vec(newPos.x, newPos.y)
   };
   bitContainerArray.push(newBit);
+}
+
+function removeFirstBit() {
+  bitContainerArray.shift();
 }
 
 function drawPlayerBit() {
@@ -228,15 +324,24 @@ function drawBitArray() {
     // Display the bit container
     char("c", bit.pos, { scale: vec(4, 4) });
 
-    // Display the number inside the bit container
-    if (!bit.activated) {
-      color("light_black")
-    }
+    // Display the number inside the bit container it the container is not blank
+    if (!bit.blank){
+      if (!bit.activated) {
+        color("light_black")
+      }
 
-    if (bit.expectedValue) {
-      char("b", bit.pos, { scale: vec(2, 2) });
-    } else {
-      char("a", bit.pos, { scale: vec(2, 2) });
+      if (bit.expectedValue) {
+        char("b", bit.pos, { scale: vec(2, 2) });
+      } else {
+        char("a", bit.pos, { scale: vec(2, 2) });
+      }
     }
   }
+}
+
+/**
+ * @param {Number} timeLimit
+ */
+function setTimer(timeLimit) {
+  timer.timeLimit = timeLimit
 }
